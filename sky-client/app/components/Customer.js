cbf1a6ba7f24b873fc929c6fa2f0c5a6685e6eb5
Mihailovs <artjoms.mihailovs@accenture.com>
2017-06-22 14:34:16 +0300

    FIxed bug with radio buttons not firing customer selection handler. Added gitignore file.

--- sky-client/app/components/Customer.js
+++ sky-client/app/components/Customer.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types'
 export default class Customer extends React.Component {
     constructor(props) {
         super(props);
-        
+        this.state = { customerSelected: ''};
         this.chooseUser = this.chooseUser.bind(this);
     }
 
     chooseUser(e) {
+        console.log('!!! chooseUser works !!!');
+        this.setState({ customerSelected: e.currentTarget.value});
         this.props.getUser(e.currentTarget.value);
     }
 
@@ -19,10 +21,16 @@ export default class Customer extends React.Component {
                     <h2>Choose customer</h2>
                 </div>
                 <div className="radio">
-                    <label><input type="radio" name="user" defaultValue="london" onChange={this.chooseUser}/>London</label>
+                    <label><input type="radio" name="user" 
+                                defaultValue="london" 
+                                checked={this.state.customerSelected === 'london'}
+                                onChange={this.chooseUser}/>London</label>
                 </div>
                 <div className="radio">
-                    <label><input type="radio" name="user" defaultValue="liverpool" onChange={this.chooseUser}/>Liverpool</label>
+                    <label><input type="radio" name="user" 
+                                defaultValue="liverpool" 
+                                checked={this.state.customerSelected === 'liverpool'}
+                                onChange={this.chooseUser}/>Liverpool</label>
                 </div>
             </div>
         );
